04688265e06bbed220602c96707496711ba384d3
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2024-10-05 17:36:18 -0400

    Add initialization files for direnv
    * .bash/interactive.d/95-direnv.bash: Add initialization for BASH.
    * .zsh/interactive.d/95-direnv.zsh: Add initialization for ZSH.

---
+++ .bash/interactive.d/95-direnv.bash
@@ -0,0 +1,6 @@
+# -*- mode: sh; sh-shell: bash -*-
+# vim:ft=sh:
+
+# /bin/echo -e '\033[1;32mIn .bash/interactive.d/95-direnv.bash\033[0m'
+
+eval "$(direnv hook bash)"
---
+++ .zsh/interactive.d/95-direnv.zsh
@@ -0,0 +1,6 @@
+# -*- mode: sh; sh-shell: zsh -*-
+# vim:ft=sh:
+
+# /bin/echo -e '\033[1;32mIn .zsh/interactive.d/95-direnv.zsh\033[0m'
+
+eval "$(direnv hook zsh)"
